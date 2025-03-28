b12774cb4dda8833501383bc115c98cc08f389b9
Tejas-Naik <rntejas2005@gmail.com>
2025-03-28 23:43:07 +0530

    Implement interview session

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import DisplayTechIcons from "./DisplayTechIcons";
 
 function InterviewCard({
-  interviewId,
+  id,
   userId,
   role,
   type,
@@ -60,11 +60,7 @@ function InterviewCard({
           <DisplayTechIcons techStack={techstack} />
           <Button className="btn-primary">
             <Link
-              href={
-                feedback
-                  ? `/interview/${interviewId}/feedback`
-                  : `/interview/${interviewId}`
-              }
+              href={feedback ? `/interview/${id}/feedback` : `/interview/${id}`}
             >
               {feedback ? "Check Feedback" : "View Interview"}
             </Link>
